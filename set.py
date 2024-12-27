170e7764baf4dfe4c83ffeb860281e3c1cadf8d6
Abdullah <abdullahrehan2010@gmail.com>
2024-12-27 14:45:27 +0500

    Add files via upload

---
+++ set.py
@@ -0,0 +1,7 @@
+set1= {1, 1,2,2,2,3,4} 
+set2={4,5,2,8,9}
+print("original set is", set1)
+print("union operation: ",set1.union(set2))
+print("intersection: ",set1. intersection(set2))
+print("difference", set1. difference(set2))
+print("symmetric difference", set1. symmetric_difference(set2))
---
+++ tuple.py
@@ -0,0 +1,7 @@
+tuple1=(1, 1,2,3,4)
+print( "original tuple is", tuple1)
+print("Oth index", tuple1[0])
+print(tuple1[0:2])
+print(len(tuple1))
+    for i in tuple1:
+        print(i)
